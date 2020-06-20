c5bd5db8f0450afe34ea15a8395b3656edf21d52
Dave Caulton <dcaulton@gmail.com>
2020-06-20 18:01:53 -0500

    add data migration for ping operation

--- django/cv_worker/operations/api.py
+++ django/cv_worker/operations/api.py
@@ -4,5 +4,7 @@ from cv_worker.operations.models import Operation
 
 class OperationViewSet(viewsets.ViewSet):
     def list(self, request):
-        operations_list = Operation.objects.all()
+        operations_list = []
+        for operation in Operation.objects.all():
+            operations_list = operation.__str__()
         return Response({"operations": operations_list})
--- django/cv_worker/operations/models.py
+++ django/cv_worker/operations/models.py
@@ -1,4 +1,5 @@
 import uuid
+import datetime
 from django.db import models
 
 class Operation(models.Model):
@@ -8,3 +9,13 @@ class Operation(models.Model):
     content = models.TextField(null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        ret_obj = {
+            'id': str(self.id),
+            'name': self.name,
+            'description': self.description,
+            'created_on': datetime.datetime.strftime(self.created_on, '%Y-%m-%d %H:%M:%S'),
+            'updated_on': datetime.datetime.strftime(self.updated_on, '%Y-%m-%d %H:%M:%S'),
+        }
+        return ret_obj
