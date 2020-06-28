c479872c5411189cf0f98be1237df1d405b8c2ff
Dave Caulton <dcaulton@gmail.com>
2020-06-28 15:03:57 -0500

    fleshing out how settings get updated with a server restart and error handling

--- django/server/api.py
+++ django/server/api.py
@@ -7,8 +7,7 @@ import shutil
 
 class ServerSettingsViewSet(viewsets.ViewSet):
     def list(self, request):
-        resp = settings.CV_WORKER
-        return Response(resp)
+        return Response({'CV_WORKER': settings.CV_WORKER})
 
     def patch(self, request, pk=None):
         if not request.data.get('CV_WORKER'):
@@ -22,7 +21,16 @@ class ServerSettingsViewSet(viewsets.ViewSet):
         settings_filename = os.path.join(settings.BASE_DIR, 'server', 'settings.json')
         backup_settings_filename = os.path.join(settings.BASE_DIR, 'server', 'settings.json.bak')
         shutil.copyfile(settings_filename, backup_settings_filename)
-        with open (settings_filename, 'w') as outfile:
-            json.dump(build_settings, outfile, indent=2)
+        try:
+            with open (settings_filename, 'w') as outfile:
+                json.dump(build_settings, outfile, indent=2)
+            self.restartServer()
+            return Response()
+        except Exception as err:
+            return Response('problem parsing the json and restarting the server: {}'.format(err), status=400)
 
-        return Response()
+
+    def restartServer(self):
+        # TODO write this logic, it's not needed while we're using runserver in development mode.
+        # I think this is a good use for a broadcast on the system or work pool queue
+        pass
